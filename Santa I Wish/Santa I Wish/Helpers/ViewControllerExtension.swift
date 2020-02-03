834478a978d00e4fe54be99056ef92626588bd8f
Wilma <kima_92@fastmail.com>
2020-02-03 16:05:30 -0500

    ViewController Extension to implement hide Keyboard functionality

---
+++ Santa I Wish/Santa I Wish/Helpers/ViewControllerExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ViewControllerExtension.swift
+//  Santa I Wish
+//
+//  Created by macbook on 2/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
