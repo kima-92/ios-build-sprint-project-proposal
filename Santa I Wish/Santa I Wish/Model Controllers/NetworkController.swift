da3e73ffd107615bf90b32a7a5e917052cccca4e
Wilma <kima_92@fastmail.com>
2020-02-03 14:33:25 -0500

    Added Firebase and modified .gitignore

---
+++ Santa I Wish/Santa I Wish/Model Controllers/NetworkController.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkController.swift
+//  Santa I Wish
+//
+//  Created by macbook on 2/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkController {
+    private let baseURL = URL(string: "https://santaiwishbwunit4.firebaseio.com/")!
+    
+}
